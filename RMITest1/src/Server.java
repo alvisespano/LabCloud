66ef7afcbea0dd3f4a6d6e436143b57875dfc122
Alvise Spanò <spano.alvise@gmail.com>
2025-04-03 11:59:56 +0200

    Lesson #1 updated

--- RMITest1/src/Server.java
+++ RMITest1/src/Server.java
@@ -6,6 +6,8 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class Server extends UnicastRemoteObject implements MyRemote {
 
+    private String storage;
+
     public Server() throws RemoteException {
         super();
     }
@@ -17,17 +19,32 @@ public class Server extends UnicastRemoteObject implements MyRemote {
         return r;
     }
 
+    @Override
+    public void store(String s) throws RemoteException {
+        System.out.printf("store(%s)\n", s);
+        storage = s;
+    }
+
+    @Override
+    public String retrieve() throws RemoteException {
+        System.out.printf("retrieve() = \"%s\"\n", storage);
+        return storage;
+    }
+
     public static void main(String[] args) {
         try {
+            // instantiates the Server class
             Server server = new Server();
+
+            // register the server
 
             // mode 1: use the global registry
             // 'rmiregistry' program must be running in the background on your machine
-            Naming.rebind("rmi://localhost:5000/sqrt", server);
+            //Naming.rebind("rmi://localhost:5000/myserver", server);
 
             // mode 2: create registry and register the service name
-            //Registry reg = LocateRegistry.createRegistry(5000);
-            //reg.rebind("sqrt", server);
+            Registry reg = LocateRegistry.createRegistry(5000);
+            reg.rebind("myserver", server);
 
             System.out.printf("server is up...\n");
 
